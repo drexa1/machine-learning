41bd263db9f48bc0f6129e274bea4cdff4cdc6c8
Diego Ruiz <drexa1@hotmail.com>
2016-11-12 17:06:35 +0100

    Naive Bayes done

--- Naive Bayes/sms_spam.R
+++ Naive Bayes/sms_spam.R
@@ -1,5 +1,7 @@
 library("tm")
 library("wordcloud")
+library("e1071")
+library("gmodels")
 
 cat("\n")
 cat("---------------------------------------------------------- \n")
@@ -22,6 +24,14 @@ prompt_num <- function(str) {
     }
     return (as.integer(input))
 }
+prompt_ys <- function(str) {
+    input <- readline(str)
+    input <- ifelse(grepl("y|n", input), as.character(input), -1)
+    if(!is.character(input)) {
+        prompt_norm(str)
+    }
+    return (as.character(input))
+}
 
 # Convert type to factor
 sms_raw$type <- factor(sms_raw$type)
@@ -32,15 +42,15 @@ sms_corpus <- VCorpus(VectorSource(sms_raw$text))
 cat("*** Generated volatile corpus \n")
 
 # Apply transformations manually (order matters)
-sms_corpus_clean <- tm_map(sms_corpus, content_transformer(tolower))
-sms_corpus_clean <- tm_map(sms_corpus, removePunctuation)
-sms_corpus_clean <- tm_map(sms_corpus, removeNumbers)
-sms_corpus_clean <- tm_map(sms_corpus_clean, stripWhitespace)
+# sms_corpus_clean <- tm_map(sms_corpus, content_transformer(tolower))
+# sms_corpus_clean <- tm_map(sms_corpus, removePunctuation)
+# sms_corpus_clean <- tm_map(sms_corpus, removeNumbers)
+# sms_corpus_clean <- tm_map(sms_corpus_clean, stripWhitespace)
 # Irrelevant words
-sms_corpus_clean <- tm_map(sms_corpus_clean, removeWords, stopwords())
+# sms_corpus_clean <- tm_map(sms_corpus_clean, removeWords, stopwords())
 # Stemming
-sms_corpus_clean <- tm_map(sms_corpus_clean, stemDocument)
-cat("*** Corpus cleaned \n")
+# sms_corpus_clean <- tm_map(sms_corpus_clean, stemDocument)
+# cat("*** Corpus cleaned \n")
 
 example <- sample(1:length(sms_corpus), 1)
 cat(as.character(sms_corpus[[example]]))
@@ -48,10 +58,10 @@ cat("\n")
 cat(as.character(sms_corpus_clean[[example]]))
 
 # Tokenize into term matrix
-sms_dtm <- DocumentTermMatrix(sms_corpus_clean)
+# sms_dtm <- DocumentTermMatrix(sms_corpus_clean)
 # Alternative order of transformations
 if(FALSE)
-sms_dtm2 <- DocumentTermMatrix(sms_corpus, 
+sms_dtm <- DocumentTermMatrix(sms_corpus, 
                 control = list(tolower = TRUE, 
                                removeNumbers = TRUE, 
                                stopwords = TRUE, 
@@ -84,9 +94,37 @@ print(prop.table(table(sms_test_labels)))
 # Subset for wordclouds
 spam <- subset(sms_raw, type=="spam")
 ham <- subset(sms_raw, type=="ham")
-cat("\n *** Subset data set for wordclouds \n")
+cat("\n*** Subset ham/spam for wordclouds \n")
 
 # 50 represents 1% of the whole dataset
 par(mfrow=c(1,2)) 
-wordcloud(spam$text, max.words=50, random.order=FALSE, scale=c(3,0.5))
-wordcloud(ham$text, max.words=50, random.order=FALSE, scale=c(3,0.5))
+wordcloud(spam$text, max.words=50, random.order=FALSE, scale=c(2,0.3))
+wordcloud(ham$text, max.words=50, random.order=FALSE, scale=c(2,0.3))
+
+# Reduce the sparse matrix
+sms_freq_words <- findFreqTerms(sms_dtm_train, 5)
+sms_dtm_freq_train <- sms_dtm_train[ , sms_freq_words]
+sms_dtm_freq_test <- sms_dtm_test[ , sms_freq_words]
+cat("*** Training and test sparse dtms reduced \n")
+
+# Turn columns into categorical
+convert_counts <- function(x) { 
+    x <- ifelse(x>0, "YES", "NO") 
+}
+sms_train <- apply(sms_dtm_freq_train, MARGIN=2, convert_counts)
+sms_test <- apply(sms_dtm_freq_test, MARGIN=2, convert_counts)
+cat("*** DTMs converted to categorical \n")
+
+# Enable Laplace estimator
+confirm <- prompt_ys("*** Enable Laplace estimator (y/n): ")
+laplace_estimator <- ifelse(confirm=="y", 1, 0)
+sms_classifier <- naiveBayes(sms_train, sms_train_labels, laplace=laplace_estimator)
+cat("*** Classifier build, predicting... \n")
+sms_test_pred <- predict(sms_classifier, sms_test)
+
+# Results
+print(CrossTable(sms_test_pred, sms_test_labels, 
+           prop.chisq=FALSE, 
+           prop.t=FALSE, 
+           dnn = c('Predicted', 'Actual')))
+
