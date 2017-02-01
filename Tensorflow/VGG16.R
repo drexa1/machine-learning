4bf02a4f158f7e1940ffd08818d88e297252c1ab
Diego Ruiz <diego.ruiz@mapp.com>
2017-02-01 12:01:19 +0100

    Refactoring VGG16

--- Tensorflow/VGG16.R
+++ Tensorflow/VGG16.R
@@ -13,26 +13,26 @@ cat("------------------------------------------------ \n")
 cat(" Classification using Convoluted neural networks \n")
 cat("------------------------------------------------ \n")
 
-setwd("C:/Users/drexa/git/MachineLearning/Tensorflow")
-# setwd("C:/Users/dr186049/git/MachineLearning/Tensorflow")
+# setwd("C:/Users/drexa/git/MachineLearning/Tensorflow")
+setwd("C:/Users/dr186049/git/MachineLearning/Tensorflow")
 
 # Import image
-img1 <- readJPEG('./image.jpg')
-size = dim(img1)
-img_array = array(255 * img1, c(1, size[1], size[2], size[3]))
-
-slim = tf$contrib$slim
-# Build the model from scratch
-tf$reset_default_graph()
+img <- readJPEG('./image.jpg')
+size = dim(img)
+img_array = array(255 * img, c(1, size[1], size[2], size[3]))
 
 # Tensor placeholder
-images = tf$placeholder(tf$float32, shape(NULL, NULL, NULL, 3))
-images_scaled = tf$image$resize_images(images, shape(224, 224))
+placeholder = tf$placeholder(tf$float32, shape(NULL, NULL, NULL, 3))
+placeholder_scaled = tf$image$resize_images(placeholder, shape(224, 224))
 cat("*** Image imported and scaled \n")
+
+# Build the model from scratch
+tf$reset_default_graph()
+slim = tf$contrib$slim
 
 # Definition of VGG16 network
 cat("*** Generating convnet model... \n")
-vgg_16 = slim$conv2d(images_scaled, 64, shape(3, 3), scope = 'vgg_16/conv1/conv1_1') %>%
+vgg_16 = slim$conv2d(placeholder_scaled, 64, shape(3, 3), scope = 'vgg_16/conv1/conv1_1') %>%
          slim$conv2d(64, shape(3, 3), scope = 'vgg_16/conv1/conv1_2') %>%
          slim$max_pool2d(shape(2, 2), scope = 'vgg_16/pool1') %>%
 
@@ -56,10 +56,12 @@ vgg_16 = slim$conv2d(images_scaled, 64, shape(3, 3), scope = 'vgg_16/conv1/conv1
          slim$max_pool2d(shape(2, 2), scope = 'vgg_16/pool5') %>%
 
          slim$conv2d(4096, shape(7, 7), padding = 'VALID', scope = 'vgg_16/fc6') %>%
+
          slim$conv2d(4096, shape(1, 1), scope = 'vgg_16/fc7') %>%
 
          # Setting the activation_fn=NULL does not work, so we get a ReLU
-slim$conv2d(1000, shape(1, 1), scope = 'vgg_16/fc8') %>%
+         slim$conv2d(1000, shape(1, 1), scope = 'vgg_16/fc8') %>%
+
          tf$squeeze(shape(1, 2), name = 'vgg_16/fc8/squeezed')
 
 # Tensorboard graph
@@ -85,13 +87,13 @@ idx = sort.int(vgg_16_vals, index.return = TRUE, decreasing = TRUE)$ix[1:5]
 labels = read_delim("labels.txt", "\t", escape_double = FALSE, trim_ws = TRUE, col_names = FALSE)
 
 # Plot
-g = rasterGrob(img1, interpolate = TRUE)
+g = rasterGrob(img, interpolate = TRUE)
 text = ""
 for (i in idx) {
     text = paste0(text, labels[i,][[1]], " ", round(probs[i], 2), "\n")
 }
 p = print(ggplot(data.frame(d = 1:3)) +
-          annotation_custom(g) +
-          annotate('text', x = 0.05, y = 0.05, label = text, size = 5, hjust = 0, vjust = 0, color = 'red') +
-          xlim(0, 1) +
-          ylim(0, 1))
+                 annotation_custom(g) +
+                 annotate('text', x = 0.05, y = 0.05, label = text, size = 5, hjust = 0, vjust = 0, color = 'red') +
+                 xlim(0, 1) +
+                 ylim(0, 1))
