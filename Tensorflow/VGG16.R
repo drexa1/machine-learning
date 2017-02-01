8793c18a8048fcc33ae5aac3382561b81368d26a
Diego Ruiz <diego.ruiz@mapp.com>
2017-02-01 12:26:44 +0100

    VGG16 code beautification

--- Tensorflow/VGG16.R
+++ Tensorflow/VGG16.R
@@ -1,12 +1,9 @@
-library("tensorflow")
-library("magrittr")
-library("jpeg")
-library("readr")
-library("grid")
-library("ggplot2")
+library("tensorflow") # Google Tensorflow R wrapper
+library("magrittr")   # Ceci n'est pas un pipe
+library("ggplot2")    # Graphics plotting
 
 options(warn = -1)
-set.seed(123)
+set.seed(123) # Experiment reproducibility
 
 cat("\n")
 cat("------------------------------------------------ \n")
@@ -17,7 +14,7 @@ cat("------------------------------------------------ \n")
 setwd("C:/Users/dr186049/git/MachineLearning/Tensorflow")
 
 # Import image
-img <- readJPEG('./image.jpg')
+img <- jpeg::readJPEG('./image.jpg')
 size = dim(img)
 img_array = array(255 * img, c(1, size[1], size[2], size[3]))
 
@@ -28,39 +25,38 @@ cat("*** Image imported and scaled \n")
 
 # Build the model from scratch
 tf$reset_default_graph()
-slim = tf$contrib$slim
+tfSlim = tf$contrib$slim
 
 # Definition of VGG16 network
 cat("*** Generating convnet model... \n")
-vgg_16 = slim$conv2d(placeholder_scaled, 64, shape(3, 3), scope = 'vgg_16/conv1/conv1_1') %>%
-         slim$conv2d(64, shape(3, 3), scope = 'vgg_16/conv1/conv1_2') %>%
-         slim$max_pool2d(shape(2, 2), scope = 'vgg_16/pool1') %>%
+vgg_16 = tfSlim$conv2d(placeholder_scaled, 64, shape(3, 3), scope = 'vgg_16/conv1/conv1_1') %>%
+         tfSlim$conv2d(64, shape(3, 3), scope = 'vgg_16/conv1/conv1_2') %>%
+         tfSlim$max_pool2d(shape(2, 2), scope = 'vgg_16/pool1') %>%
 
-         slim$conv2d(128, shape(3, 3), scope = 'vgg_16/conv2/conv2_1') %>%
-         slim$conv2d(128, shape(3, 3), scope = 'vgg_16/conv2/conv2_2') %>%
-         slim$max_pool2d(shape(2, 2), scope = 'vgg_16/pool2') %>%
+         tfSlim$conv2d(128, shape(3, 3), scope = 'vgg_16/conv2/conv2_1') %>%
+         tfSlim$conv2d(128, shape(3, 3), scope = 'vgg_16/conv2/conv2_2') %>%
+         tfSlim$max_pool2d(shape(2, 2), scope = 'vgg_16/pool2') %>%
 
-         slim$conv2d(256, shape(3, 3), scope = 'vgg_16/conv3/conv3_1') %>%
-         slim$conv2d(256, shape(3, 3), scope = 'vgg_16/conv3/conv3_2') %>%
-         slim$conv2d(256, shape(3, 3), scope = 'vgg_16/conv3/conv3_3') %>%
-         slim$max_pool2d(shape(2, 2), scope = 'vgg_16/pool3') %>%
+         tfSlim$conv2d(256, shape(3, 3), scope = 'vgg_16/conv3/conv3_1') %>%
+         tfSlim$conv2d(256, shape(3, 3), scope = 'vgg_16/conv3/conv3_2') %>%
+         tfSlim$conv2d(256, shape(3, 3), scope = 'vgg_16/conv3/conv3_3') %>%
+         tfSlim$max_pool2d(shape(2, 2), scope = 'vgg_16/pool3') %>%
 
-         slim$conv2d(512, shape(3, 3), scope = 'vgg_16/conv4/conv4_1') %>%
-         slim$conv2d(512, shape(3, 3), scope = 'vgg_16/conv4/conv4_2') %>%
-         slim$conv2d(512, shape(3, 3), scope = 'vgg_16/conv4/conv4_3') %>%
-         slim$max_pool2d(shape(2, 2), scope = 'vgg_16/pool4') %>%
+         tfSlim$conv2d(512, shape(3, 3), scope = 'vgg_16/conv4/conv4_1') %>%
+         tfSlim$conv2d(512, shape(3, 3), scope = 'vgg_16/conv4/conv4_2') %>%
+         tfSlim$conv2d(512, shape(3, 3), scope = 'vgg_16/conv4/conv4_3') %>%
+         tfSlim$max_pool2d(shape(2, 2), scope = 'vgg_16/pool4') %>%
 
-         slim$conv2d(512, shape(3, 3), scope = 'vgg_16/conv5/conv5_1') %>%
-         slim$conv2d(512, shape(3, 3), scope = 'vgg_16/conv5/conv5_2') %>%
-         slim$conv2d(512, shape(3, 3), scope = 'vgg_16/conv5/conv5_3') %>%
-         slim$max_pool2d(shape(2, 2), scope = 'vgg_16/pool5') %>%
+         tfSlim$conv2d(512, shape(3, 3), scope = 'vgg_16/conv5/conv5_1') %>%
+         tfSlim$conv2d(512, shape(3, 3), scope = 'vgg_16/conv5/conv5_2') %>%
+         tfSlim$conv2d(512, shape(3, 3), scope = 'vgg_16/conv5/conv5_3') %>%
+         tfSlim$max_pool2d(shape(2, 2), scope = 'vgg_16/pool5') %>%
 
-         slim$conv2d(4096, shape(7, 7), padding = 'VALID', scope = 'vgg_16/fc6') %>%
+         tfSlim$conv2d(4096, shape(7, 7), padding = 'VALID', scope = 'vgg_16/fc6') %>%
 
-         slim$conv2d(4096, shape(1, 1), scope = 'vgg_16/fc7') %>%
+         tfSlim$conv2d(4096, shape(1, 1), scope = 'vgg_16/fc7') %>%
 
-         # Setting the activation_fn=NULL does not work, so we get a ReLU
-         slim$conv2d(1000, shape(1, 1), scope = 'vgg_16/fc8') %>%
+         tfSlim$conv2d(1000, shape(1, 1), scope = 'vgg_16/fc8') %>% # activation_fn=NULL does not work, we get a ReLU here
 
          tf$squeeze(shape(1, 2), name = 'vgg_16/fc8/squeezed')
 
@@ -68,11 +64,10 @@ vgg_16 = slim$conv2d(placeholder_scaled, 64, shape(3, 3), scope = 'vgg_16/conv1/
 cat("*** Model rendered to tensorboard \n")
 tf$summary$FileWriter('./vgg16', tf$get_default_graph())$close()
 
-# Restore weights
+# Restore trained VGG16 weights
 cat("*** Restoring weights\n")
-restorer = tf$train$Saver()
 session = tf$Session()
-# tf$global_variables_initializer()
+restorer = tf$train$Saver()
 restorer$restore(session, './vgg_16.ckpt')
 
 # Feed the placeholder
@@ -84,10 +79,10 @@ probs = exp(vgg_16_vals) / sum(exp(vgg_16_vals))
 idx = sort.int(vgg_16_vals, index.return = TRUE, decreasing = TRUE)$ix[1:5]
 
 # Read class labels
-labels = read_delim("labels.txt", "\t", escape_double = FALSE, trim_ws = TRUE, col_names = FALSE)
+labels = readr::read_delim("labels.txt", "\t", escape_double = FALSE, trim_ws = TRUE, col_names = FALSE)
 
 # Plot
-g = rasterGrob(img, interpolate = TRUE)
+g = grid::rasterGrob(img, interpolate = TRUE)
 text = ""
 for (i in idx) {
     text = paste0(text, labels[i,][[1]], " ", round(probs[i], 2), "\n")
