df4b4b2f52b5565bcfa850beab63f4534bc5ee44
Diego Ruiz Barbero <drexa1@hotmail.com>
2018-01-05 09:54:17 +0100

    Update sndata.R

--- k-Means Clustering/sndata.R
+++ k-Means Clustering/sndata.R
@@ -5,8 +5,7 @@ cat("---------------------------------------------------------- \n")
 cat(" Clustering using k-Means \n")
 cat("---------------------------------------------------------- \n")
 
-setwd("C:/Users/drexa/git/MachineLearning/k-Means Clustering")
-# setwd("C:/Users/dr186049/git/MachineLearning/k-Means Clustering")
+setwd("C:/Users/drexa/git/machine-learning/k-Means Clustering")
 
 # Import data
 teens <- read.csv("../datasets/sndata.csv")
