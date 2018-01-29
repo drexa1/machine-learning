2751e90814c4f7b672312ef304243ea4a3e4b907
Diego Ruiz Barbero <drexa1@hotmail.com>
2018-01-29 15:19:27 +0100

    Update mcmc.py
    Documentation

--- Monte Carlo/mcmc.py
+++ Monte Carlo/mcmc.py
@@ -2,6 +2,10 @@ import csv, math
 from random import randint, random, shuffle
 import matplotlib.pyplot as plt, numpy as np
 
+# For small solution spaces it’s better to lower the starting temperature and increase the cooling rate, 
+# as it will reduce the simulation time, without lose of quality.
+# For bigger solution spaces choose a higher starting temperature and a small cooling rate, 
+# as there will be more local minima.
 def main():
 	cities = get_cities()
 
@@ -22,11 +26,10 @@ def main():
 	print_total_distance(route_4)
 
 	print("\nUsing MCMC with Simulated Annealing, MAX_ITER = 10,000 and c = 70")
-	route_5 = MCMC_SA(cities, MAX_ITER=10000, c=70)
+	route_5 = MCMC_SA(cities, MAX_ITER=10000, c=100)
 	print_total_distance(route_5)
 
-# Markov Chain Monte Carlo method of solving the traveling salesman problem
-# cities is a dict of city-coordinate pairs
+# Markov Chain Monte Carlo method for TSP.
 # MAX_ITER is the number of iterations
 # T is the Markov Chain Monte Carlo temperature
 # Lower temperature results in a slower mixing time, and is more likely to
@@ -55,7 +58,7 @@ def MCMC(cities, MAX_ITER=10000, T=10):
 
 	return best
 
-# This is MCMC with Simulated Annealing (i.e. T decreases over time)
+# MCMC with Simulated Annealing (T decreases over time over a cooling rate)
 def MCMC_SA(cities, MAX_ITER=10000, c=70):
 	route = list(cities.items())
 	best  = list(route)
@@ -65,13 +68,15 @@ def MCMC_SA(cities, MAX_ITER=10000, c=70):
 	best_distance  = total_distance(best)
 
 	for t in range(1, MAX_ITER+1):
-		T = c / math.sqrt(t)
+		T = c / math.sqrt(t) # cuadratic cooling rate
 		new_route = create_new_route(route)
 		new_distance   = total_distance(new_route)
 		delta_distance = new_distance - curr_distance
 
+		# either is an improve
 		if (delta_distance < 0) or \
-		(T > 0 and random() < math.exp(-1 * delta_distance / T)):
+		# or else Boltzmann function of probability distribution lower than random value in 0-1 range
+		(T > 0 and random() < math.exp(-1 * delta_distance / T)): 
 			route = new_route
 			curr_distance = new_distance
 
@@ -92,7 +97,7 @@ def get_cities(cities_csv_file="cities.csv"):
 		cities = {line[0]:[float(line[1]), float(line[2])] for line in city_reader}
 		return cities
 
-# route is a list of key-value tuples, e.g. ('City', [latitude, longitude])
+# route is a list of key-value tuples ('City', [latitude, longitude])
 def create_new_route(route):
 	new_route = list(route)
 
@@ -126,7 +131,7 @@ def print_total_distance(route, verbose=1):
 	print("The total distance is {} km.".format(int(dist)))
 	return dist
 
-# route is a list of key-value tuples, e.g. ('City', [latitude, longitude])
+# route is a list of key-value tuples, ('City', [latitude, longitude])
 def total_distance(route):
 	dist = 0.0
 	for i in range(len(route) - 1): # skip last element
@@ -141,19 +146,19 @@ def total_distance(route):
 # haversine distance between x and y
 # x and y are kv tuples of the form ('City', [latitude, longitude])
 def distance(x, y):
-  # convert degrees to radians
-  lat_x, lon_x = map(math.radians, x[1])
-  lat_y, lon_y = map(math.radians, y[1])
+    # convert degrees to radians
+    lat_x, lon_x = map(math.radians, x[1])
+    lat_y, lon_y = map(math.radians, y[1])
 
-  # haversine of distance / radius
-  h = (haversine(lat_y - lat_x) + math.cos(lat_x) * math.cos(lat_y) * haversine(lon_y - lon_x))
-  r = 6371 # Radius of earth in kilometers
-  d = 2.0 * r * math.asin(math.sqrt(h))
-  return d
+    # haversine of distance / radius
+    h = (haversine(lat_y - lat_x) + math.cos(lat_x) * math.cos(lat_y) * haversine(lon_y - lon_x))
+    r = 6371 # radius of earth in kilometers
+    d = 2.0 * r * math.asin(math.sqrt(h))
+    return d
 
-# the haversine function
+# haversine function
 def haversine(theta):
-	return math.sin(theta / 2.0) ** 2
+    return math.sin(theta / 2.0) ** 2
 
 if __name__ == "__main__":
     # execute only if run as a script
