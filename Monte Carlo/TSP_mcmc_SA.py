266ae96b25e8e3bb87db0f748fe69b0cb82f57fc
Diego Ruiz <drexa1@hotmail.com>
2018-02-03 22:16:54 +0100

    Logging for simulated annealing

--- Monte Carlo/TSP_mcmc_SA.py
+++ Monte Carlo/TSP_mcmc_SA.py
@@ -1,6 +1,11 @@
 import csv, math
-from random import randint, random, shuffle
-import matplotlib.pyplot as plt, numpy as np
+from random import random, randint, shuffle
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+
+
 
 # For small solution spaces it’s better to lower the starting temperature and increase the cooling rate, 
 # as it will reduce the simulation time, without lose of quality.
@@ -9,32 +14,32 @@ import matplotlib.pyplot as plt, numpy as np
 def main():
 	cities = get_cities()
 
-	print("\nUsing MCMC with MAX_ITER = 10,000 and T = 0")
-	route_1 = MCMC(cities, MAX_ITER=10000, T=0)
-	print_total_distance(route_1)
+	logger.info("Using MCMC with T = 0")
+	route_1 = MCMC(cities, STEPS=10_000, T=0)
+	print_total_distance(route_1, False)
 
-	print("\nUsing MCMC with MAX_ITER = 10,000 and T = 1")
-	route_2 = MCMC(cities, MAX_ITER=10000, T=1)
-	print_total_distance(route_2)
+	logger.info("Using MCMC with T = 1")
+	route_2 = MCMC(cities, STEPS=10_000, T=1)
+	print_total_distance(route_2, False)
 
-	print("\nUsing MCMC with MAX_ITER = 10,000 and T = 10")
-	route_3 = MCMC(cities, MAX_ITER=10000, T=10)
-	print_total_distance(route_3)
+	logger.info("Using MCMC with T = 10")
+	route_3 = MCMC(cities, STEPS=10_000, T=10)
+	print_total_distance(route_3, False)
 
-	print("\nUsing MCMC with MAX_ITER = 10,000 and T = 100")
-	route_4 = MCMC(cities, MAX_ITER=10000, T=100)
-	print_total_distance(route_4)
+	logger.info("Using MCMC with T = 100")
+	route_4 = MCMC(cities, STEPS=10_000, T=100)
+	print_total_distance(route_4, False)
 
-	print("\nUsing MCMC with Simulated Annealing, MAX_ITER = 10,000 and c = 100")
-	route_5 = MCMC_SA(cities, MAX_ITER=10000, c=100)
-	print_total_distance(route_5)
+	logger.info("Using MCMC with simulated annealing, and c = 100")
+	route_5 = MCMC_SA(cities, STEPS=10_000, c=100)
+	print_total_distance(route_5, True)
 
 # Markov Chain Monte Carlo method for TSP.
-# MAX_ITER is the number of iterations
+# STEPS is the number of iterations
 # T is the Markov Chain Monte Carlo temperature
 # Lower temperature results in a slower mixing time, and is more likely to
 # result in a local optimum, but also requires fewer iterations
-def MCMC(cities, MAX_ITER=10000, T=10):
+def MCMC(cities, STEPS=10_000, T=10):
 	route = list(cities.items())
 	best  = list(route)
 	shuffle(route)
@@ -42,8 +47,8 @@ def MCMC(cities, MAX_ITER=10000, T=10):
 	curr_distance  = total_distance(route)
 	best_distance  = total_distance(best)
 
-	for _ in range(MAX_ITER):
-		new_route = create_new_route(route)
+	for _ in range(STEPS):
+		new_route = create_new_route(route, False)
 		new_distance   = total_distance(new_route)
 		delta_distance = new_distance - curr_distance
 
@@ -59,27 +64,32 @@ def MCMC(cities, MAX_ITER=10000, T=10):
 	return best
 
 # MCMC with Simulated Annealing (T decreases over time over a cooling rate)
-def MCMC_SA(cities, MAX_ITER=10000, c=100):
+def MCMC_SA(cities, STEPS=10_000, c=100):
 	route = list(cities.items())
-	best  = list(route)
+	best = list(route)
 	shuffle(route)
 
-	curr_distance  = total_distance(route)
-	best_distance  = total_distance(best)
+	curr_distance = total_distance(route)
+	best_distance = total_distance(best)
 
-	for t in range(1, MAX_ITER+1):
-		T = c / math.sqrt(t) # cuadratic cooling rate
-		new_route = create_new_route(route)
-		new_distance   = total_distance(new_route)
+	for t in range(1, STEPS+1):
+		T = c / math.sqrt(t) # quadratic cooling rate
+		new_route = create_new_route(route, True)
+		new_distance = total_distance(new_route)
 		delta_distance = new_distance - curr_distance
 
 		# either is an improve
-		if (delta_distance < 0) or \
-		# or else Boltzmann function of probability distribution lower than random value in 0-1 range.
+		# or else Boltzmann function of probability distribution lower than random value in 0-1 range
+		# (it is not an improvement but I still grant myself to keep exploring around).
 		# The Boltzmann distribution is a probability distribution that gives the probability of a certain state 
 		# as a function of that state’s energy and temperature.
-		# H(T) = ∑j exp(−Ej / kT)
-		(T > 0 and random() < math.exp(-1 * delta_distance / T)): 
+		# H(T) = ∑j e^(−Ej / kT)
+		rand = random()
+		boltzmann = (-1 * delta_distance / T)
+		logger.debug("Current distance: {}, new distance: {} [{}] | Temperature: {}, random: {}, Boltzmann: {} [{}]" \
+				.format(int(curr_distance), int(new_distance), (delta_distance < 0), round(T,4), rand, math.exp(boltzmann), rand < math.exp(boltzmann)))
+
+		if (delta_distance < 0) or (T > 0 and rand < math.exp(boltzmann)): 
 			route = new_route
 			curr_distance = new_distance
 
@@ -93,19 +103,20 @@ def MCMC_SA(cities, MAX_ITER=10000, c=100):
 def get_cities(cities_csv_file="cities.csv"):
 	with open(cities_csv_file, 'rU') as cities_csv:
 		city_reader = csv.reader(cities_csv, delimiter = ',')
-		next(city_reader) # skip first line (column headings)
+		next(city_reader) # skip header
 
-		# we'll make a dict with city names as keys and coordinates as values
-		# line[0] = city name, line[1] = latitude, line[2] = longitude
+		# make a dictionary with city names as keys and coordinates as values
 		cities = {line[0]:[float(line[1]), float(line[2])] for line in city_reader}
 		return cities
 
 # route is a list of key-value tuples ('City', [latitude, longitude])
-def create_new_route(route):
+def create_new_route(route, verbose=False):
 	new_route = list(route)
 
-	# generate indices for two random cities
+	# generate indexes for two random cities
 	city_1, city_2 = (randint(0, len(route) - 1) for _ in range(2))
+	if verbose:
+		logger.debug("Swapping {} with {}.".format(route[city_1][0], route[city_2][0]))
 
 	# swap the cities
 	new_route[city_1] = route[city_2]
@@ -113,25 +124,25 @@ def create_new_route(route):
 
 	return new_route
 
-def print_total_distance(route, verbose=1):
+def print_total_distance(route, verbose=False):
 	dist = 0.0
 	for i in range(len(route) - 1): # skip last element
 		x = route[i]
 		y = route[i+1]
 		tmp = distance(x, y)
-		if verbose:
-			print("{} and {} are {} km apart.".format(x[0], y[0], int(tmp)))
 		dist += distance(x, y)
+		if verbose:
+			logger.debug("{} and {} are {} km apart.".format(x[0], y[0], int(tmp)))
 
 	# we finish where we start, so add distance between last and first
 	x = route[-1]
 	y = route[0]
 	tmp = distance(x, y)
-	if verbose:
-		print("{} and {} are {} km apart.".format(x[0], y[0], int(tmp)))
 	dist += tmp
+	if verbose:
+		logger.debug("{} and {} are {} km apart.".format(x[0], y[0], int(tmp)))
 
-	print("The total distance is {} km.".format(int(dist)))
+	logger.info("The total distance is {} km.\n".format(int(dist)))
 	return dist
 
 # route is a list of key-value tuples, ('City', [latitude, longitude])
